efd91bc523252c2656e050d223a7f13cc43b2748
Mena-U <144714889+Mena-U@users.noreply.github.com>
2023-11-19 00:37:13 +0000

    updated

--- script.js
+++ script.js
@@ -57,39 +57,21 @@ for (let i=0; i<todoItems.length; i++) {
   }
 }
 console.log(todoItems);
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+  console.log("NOT YET IMPLEMENTED"); 
 }
 
-// Function to delete a task from the array
-// It should accept a number as a parameter (id of the todo item)
-// Loop through the array of todos, and when you find the todo item with the id
-// that matches the id passed to the function, remove it from the array
-// the function does not need to return anything, though you can return
-// true or false depending on whether the item was successfully deleted
 function deleteToDoItem(todoId) {
-  // Implement the logic to remove a task here
+  
 for (let i=0; i<todoItems.length; i++) {
   if (todoItems[i].id === todoId) {
     todoItems.spliced(i, 1);
   }
 }
 console.log(todoItems);
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+  console.log("NOT YET IMPLEMENTED");
 }
 
-// Function to clear all completed tasks
-// Loop through the array of todos, and when you find a todo item that is marked
-// as completed, remove it completely from the array
 function clearCompletedTasks() {
-  // Implement the logic to clear completed tasks here
-
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+ 
+  console.log("NOT YET IMPLEMENTED"); 
 }
-
-// You can write your own tests here if you would like to test
-// your code before using the automated tests
-// For example, you could run:
-//  addToDoItem("Test ToDo"); // This should add a new todo item to the array
-//  console.log(todoItems); // This should show the todo item you added
-//  removeToDoItem(0); // This should remove the todo item with ID 0 from the array
-//  markToDoItemAsCompleted(0); // This should mark the todo item with ID 0 as completed
